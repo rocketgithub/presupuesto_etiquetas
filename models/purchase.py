61a83adc21c2662a81f0ed4e1302b686b24fe605
Rodrigo Fernandez <rodrigo@aquih.com>
2018-12-04 10:42:22 -0600

    Usar sudo para no dar permisos extra

--- models/purchase.py
+++ models/purchase.py
@@ -10,7 +10,7 @@ class PurchaseOrderLine(models.Model):
 
     por_ejecutar = fields.Monetary(string='Por ejecutar')
 
-    #Esta funcion es llamada desde el onchage, y revisa si la cuenta contable del producto coincide con alguna 
+    #Esta funcion es llamada desde el onchage, y revisa si la cuenta contable del producto coincide con alguna
     #cuenta contable de la posicion presupuestaria de la linea de presupuesto.
     def revisar_cuentas_contables(self, account_ids):
         for account_id in account_ids:
@@ -32,27 +32,26 @@ class PurchaseOrderLine(models.Model):
 
     @api.onchange('account_analytic_id', 'analytic_tag_ids', 'product_id')
     def onchange_account_analytic_id(self):
-        logging.warn('ONCHANGE_ACCOUNT_ANALYTICS_ID')
+        # logging.warn('ONCHANGE_ACCOUNT_ANALYTICS_ID')
         if self.account_analytic_id and self.product_id:
             #Hago un search de lineas de presupuesto tomando en cuenta:
             # - Fecha del pedido de compra se encuentre en el rango de fechas de la linea del presupuesto.
             # - La cuenta analitica de la linea del pedido de compra coincida con la cuenta analitica de la linea del presupuesto.
             #Existen otros dos filtros, pero esos se revisan en las funciones revisar_cuentas_contables y revisar_etiquetas.
-            lineas = self.env['crossovered.budget.lines'].search([('date_from', '<=', datetime.datetime.strptime(self.date_order, "%Y-%m-%d %H:%M:%S").date()),('date_to', '>=', datetime.datetime.strptime(self.date_order, "%Y-%m-%d %H:%M:%S").date()),('analytic_account_id', '=', self.account_analytic_id.id)])
+            lineas = self.env['crossovered.budget.lines'].sudo().search([('date_from', '<=', datetime.datetime.strptime(self.date_order, "%Y-%m-%d %H:%M:%S").date()),('date_to', '>=', datetime.datetime.strptime(self.date_order, "%Y-%m-%d %H:%M:%S").date()),('analytic_account_id', '=', self.account_analytic_id.id)])
             self.por_ejecutar = 0
             if lineas:
                 for linea in lineas:
-                    logging.getLogger('NOMBRE LINEA PRESUPUESTARIA').warn(linea.general_budget_id.name)
-                    logging.getLogger('CUENTA ANALITICA').warn(self.account_analytic_id.name)
+                    # logging.getLogger('NOMBRE LINEA PRESUPUESTARIA').warn(linea.general_budget_id.name)
+                    # logging.getLogger('CUENTA ANALITICA').warn(self.account_analytic_id.name)
                     #En estas dos condiciones se revisan los dos filtros faltantes: revisar_cuentas_contables y revisar_etiquetas.
                     if self.revisar_etiquetas(linea.analytic_tag_id):
                         if self.revisar_cuentas_contables(linea.general_budget_id.account_ids):
-                            logging.getLogger('linea.planned_amount').warn(linea.planned_amount)
-                            logging.getLogger('linea.practical_amount').warn(linea.practical_amount)
+                            # logging.getLogger('linea.planned_amount').warn(linea.planned_amount)
+                            # logging.getLogger('linea.practical_amount').warn(linea.practical_amount)
                             self.por_ejecutar = linea.planned_amount - abs(linea.practical_amount)
-                            logging.getLogger('self.por_ejecutar').warn(self.por_ejecutar)
-                    logging.getLogger('self.por_ejecutar').warn(self.por_ejecutar)
-                logging.getLogger('self.por_ejecutar').warn(self.por_ejecutar)
-            logging.getLogger('self.por_ejecutar').warn(self.por_ejecutar)
-        logging.getLogger('self.por_ejecutar').warn(self.por_ejecutar)
-
+        #                     logging.getLogger('self.por_ejecutar').warn(self.por_ejecutar)
+        #             logging.getLogger('self.por_ejecutar').warn(self.por_ejecutar)
+        #         logging.getLogger('self.por_ejecutar').warn(self.por_ejecutar)
+        #     logging.getLogger('self.por_ejecutar').warn(self.por_ejecutar)
+        # logging.getLogger('self.por_ejecutar').warn(self.por_ejecutar)
